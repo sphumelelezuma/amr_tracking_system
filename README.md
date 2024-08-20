446863dcf50fa05d69bfa99631620e23a609f2dd
sphumelelezuma <zumasphumelele5@gmail.com>
2024-08-20 15:17:30 +0200

    Initial project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# amr_tracking_system
---
+++ amr_tracking_system/amr_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AmrAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'amr_app'
